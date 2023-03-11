5c2581ea8d474a1cd87ec67788cfc11bc6b8b562
zen930409henry <127607609+zen930409henry@users.noreply.github.com>
2023-03-11 22:44:01 +0800

    Update main.cpp

--- 第4題/main.cpp
+++ 第4題/main.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 int main()
 {
-    int start_h, start_m, end_h, end_m, time, rate = 30, total = 0;
+    int start_h, start_m, end_h, end_m, t, rate = 30, total = 0;
     cin >> start_h >> start_m;
     cin >> end_h >> end_m;
-    time = (end_h - start_h - 1) * 2 + (60 - start_m + end_m) / 30;
-    for (int i = 1; i <= time; i++)
+    t = (end_h - start_h - 1) * 2 + (60 - start_m + end_m) / 30;
+    for (int i = 1; i <= t; i++)
     {
         if (i > 8) rate = 60;
         else if (i > 4) rate = 40;
