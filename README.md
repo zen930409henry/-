0ef4cd723f8898ac63f1f268c96e67da6e7e805f
吳政諺 <4b1g0027@stust.edu.tw>
2023-03-11 22:20:39 +0800

    test

---
+++ README.md
@@ -0,0 +1,6 @@
+# 英哩轉公里
+
+使用串流操作子(stream manipulators)來格式化輸出，需要先`include<iomanip>`
+
+- fixed固定顯示小數點
+- 搭配setprecision()設定小數點精度
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    int mi;
+
+    cin >> mi;
+    cout << fixed << setprecision(1) << mi * 1.6 << endl;
+    return 0;
+}
