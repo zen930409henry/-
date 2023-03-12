46b11f9f157fbc92a42dcda5cab9fa8aa53c16fc
zen930409henry <127607609+zen930409henry@users.noreply.github.com>
2023-03-12 17:02:56 +0800

    Update main.cpp

--- 第五題/main.cpp
+++ 第五題/main.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
+
 using namespace std;
 int main()
 {
-    int dec;
-    cin >> dec;
-    for (int i = 7; i >= 0; i--)
-    {
-        int k = dec >> i;
-        if (k & 1){
-            cout << "1";
-            }
-        else {
-            cout << "0";
-        }
-            
+    int decimal, binary[8] = { 0 }, i;
+
+    cin >> decimal;  //輸入十進位
+
+    if (decimal < 0) {  //如果輸入為負數，用1的補數計算
+        decimal += 256;
     }
+    for (i = 7; i >= 0; i--) {   //除2取餘，逆向排列法計算
+        binary[i] = decimal % 2;
+        decimal = decimal / 2;
+    }
+    for (i = 0; i <= 7; i++) {   //顯示二進位結果
+        cout << binary[i];
+    }
+    cout << endl;
+
     return 0;
 }
